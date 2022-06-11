041d60e005e699a1985ab683adda1fa59b0536a1
rooomer0 <80320150+rooomer0@users.noreply.github.com>
2022-06-11 04:51:55 +0200

    Comentarios y fotos y videos hecho, falta destacados, buscador y notis

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    if ($("#imagen-cargada").attr("src")=="")
+$(document).ready(function () {
+  if ($("#imagen-cargada").attr("src") == "")
     $("#imagen-cargada").hide()
 })
 
@@ -8,21 +8,49 @@ imgInp.onchange = evt => {
   if (file) {
     blah.src = URL.createObjectURL(file)
     $("#imagen-cargada").show()
-    $("#imagen-cargada").attr("src",blah.src)
+    $("#imagen-cargada").attr("src", blah.src)
   }
 }
 
-$(document).ready(function(){
-var margin = 10,
-            instance1 = new emojiButtonList( "boton-emoji", {
-                dropDownXAlign: "left",
-                textBoxID: "boton-emoji1",
-                yAlignMargin: margin,
-                xAlignMargin: margin
-            } );
+$(document).ready(function () {
+  var margin = 10,
+    instance1 = new emojiButtonList("boton-emoji", {
+      dropDownXAlign: "left",
+      textBoxID: "boton-emoji1",
+      yAlignMargin: margin,
+      xAlignMargin: margin
+    });
 
-        function emojiClickEvent( emojiText ) {
-            document.title += " " + emojiText;
-        }
+  function emojiClickEvent(emojiText) {
+    document.title += " " + emojiText;
+  }
 
-      })
+})
+
+
+$(document).ready(function () {
+  $("#contenedor-comentarios").show();
+  $("#contenedor-fotos-videos").hide();
+  $("#contenedor-destacados").hide();
+  
+  $("#seccion-comentarios").click(function () {
+    $("#contenedor-comentarios").show();
+    $("#contenedor-fotos-videos").hide();
+    $("#contenedor-destacados").hide();
+
+  });
+
+  $("#seccion-fotos-videos").click(function () {
+    $("#contenedor-comentarios").hide();
+    $("#contenedor-fotos-videos").show();
+    $("#contenedor-destacados").hide();
+
+  });
+
+  $("#seccion-destacados").click(function () {
+    $("#contenedor-comentarios").hide();
+    $("#contenedor-fotos-videos").hide();
+    $("#contenedor-destacados").show();
+
+  });
+});
