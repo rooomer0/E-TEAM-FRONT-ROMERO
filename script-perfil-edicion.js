0af40928132e3d1b748b1cc19ddbdddd1df1185c
rooomer0 <80320150+rooomer0@users.noreply.github.com>
2022-06-12 17:28:08 +0200

    Añadida pagina de buscador

--- script-perfil-edicion.js
+++ script-perfil-edicion.js
@@ -1,9 +1,7 @@
-
-
 $(document).ready(function () {
     // conexion API
     $.ajax({
-      url: 'https://eteamapp.herokuapp.com/api/users/629923ac84944079440d54e0',
+      url: 'https://eteamapp.herokuapp.com/api/users/62a4d28ac27a64a3da0995ec',
       type: 'GET',
       success: function(respuesta) {
         if (respuesta.datos.profileType.toString() == "gamer"){
@@ -18,6 +16,16 @@ $(document).ready(function () {
         $("#tipo-perfil-span").html(respuesta.datos.profileType.toString().toUpperCase());
         $("#usuario-perfil-span").html(respuesta.datos.username.toString());
         $("#correo-perfil-span").html(respuesta.datos.email.toString());
+        if (respuesta.datos.games.toString()==""){
+          $("#boton-anadir-juego").show();
+          $("#eliminar-juego-perfil").hide();
+          $("#editar-juego-perfil").hide();
+        }else {
+          $("#boton-anadir-juego").hide();
+          $("#eliminar-juego-perfil").show();
+          $("#editar-juego-perfil").show();
+        }
+        $("#games-usuario").html(respuesta.datos.games.toString());
       },
       error: function() {
           console.error("No es posible completar la operación");
